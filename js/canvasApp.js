aae4d7aaad4b17e1e76e57233694715d66287c08
darrenmwong <darrenwong06@gmail.com>
2015-02-23 10:53:47 -0800

    d3 and canvas examples

---
+++ js/canvasApp.js
@@ -0,0 +1,38 @@
+ window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      title:{
+        text: "Top Oil Reserves"    
+      },
+      animationEnabled: true,
+      axisY: {
+        title: "Reserves(MMbbl)"
+      },
+      legend: {
+        verticalAlign: "bottom",
+        horizontalAlign: "center"
+      },
+      theme: "theme2",
+      data: [
+
+      {        
+        type: "column",  
+        showInLegend: true, 
+        legendMarkerColor: "grey",
+        legendText: "MMbbl = one million barrels",
+        dataPoints: [      
+        {y: 297571, label: "Venezuela"},
+        {y: 267017,  label: "Saudi" },
+        {y: 175200,  label: "Canada"},
+        {y: 154580,  label: "Iran"},
+        {y: 116000,  label: "Russia"},
+        {y: 97800, label: "UAE"},
+        {y: 20682,  label: "US"},        
+        {y: 20350,  label: "China"}        
+        ]
+      }   
+      ]
+    });
+
+    chart.render();
+  }
